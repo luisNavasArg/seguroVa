82755eeec184713409ae579cde09902edfb984d9
luisNavasArg <lpipnavas@gmail.com>
2019-10-01 21:57:00 -0300

    creación de clase padre

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-alert()
+class SeguroVa{
+	constructor(nombre, apellido, dni, pasaporte){
+		this.nombre=nombre;
+		this.apellido=apellido;
+		this.dni=dni;
+		this.pasaporte=pasaporte;
+	}
+	subirDocumento=()=>{'foto dni y pasaporte'}
+}
+
+let asegurado = new SeguroVa();
+asegurado.nombre;
+	
